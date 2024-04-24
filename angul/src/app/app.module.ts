b8d5d0ed4e13d1849b42aa090da878137bbec881
jonatavsantos <surinegann@gmail.com>
2024-04-24 18:28:43 -0300

    Import and Export

--- angul/src/app/app.module.ts
+++ angul/src/app/app.module.ts
@@ -3,17 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardsModule } from './cards/cards.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    CardRoxoComponent,
   ],
   imports: [
     BrowserModule,
+    CardsModule,
     AppRoutingModule
   ],
   providers: [],
