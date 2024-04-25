789bb96802dee1aa4dbf513c84a11b326c3b6701
Jonata Santos <118224160+jonatavsantos@users.noreply.github.com>
2024-04-25 12:13:41 +0000

    Encadeamento Opcional

--- angul/src/app/card/card.component.ts
+++ angul/src/app/card/card.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Plano {
+  infos: Infos;
+}
+
+interface Infos {
+  tipo: string,
+  preco: number,
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
- plano = {
-  tipo: 'Simples',
-  preco: 'R$' + 100 + ',00/Mês'
- }
+ //@ts-ignore
+  plano: Plano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 100
+    }
+  }
 }
