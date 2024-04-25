bc21a13cfca9557caa0c947999bb8a3b62a8b281
Jonata Santos <118224160+jonatavsantos@users.noreply.github.com>
2024-04-25 13:24:47 +0000

    Template inside in component

--- angul/src/app/app.module.ts
+++ angul/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsModule } from './cards/cards.module';
+import { CardButtonCancelComponent } from './card-button-cancel/card-button-cancel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CardButtonCancelComponent,
   ],
   imports: [
     BrowserModule,
--- angul/src/app/card-roxo-button/card-roxo-button.component.ts
+++ angul/src/app/card-roxo-button/card-roxo-button.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-roxo-button',
-  templateUrl: './card-roxo-button.component.html',
+  //templateUrl: './card-roxo-button.component.html',
+  template: '<div class="card-roxo-button">Adquirir</div>',
   styleUrl: './card-roxo-button.component.scss'
 })
 export class CardRoxoButtonComponent {
